985eb41cb0f978c961589e688943089089f18f23
thedonald14 <74273408+thedonald14@users.noreply.github.com>
2020-11-10 15:31:26 -0800

    Update getjsonelection.py

--- getjsonelection.py
+++ getjsonelection.py
@@ -1,15 +1,24 @@
-import requests
+import urllib
+import json
 import pandas as pd
 
 def downloadvoterjson():
     """This Function downloads the JSON File for each State in the 2020 Election from the NYT """
-    
+         
     # Get all State Names from CSV
     stnames = pd.read_csv("https://raw.githubusercontent.com/jasonong/List-of-US-States/master/states.csv")
     statelist = [st for st in stnames.State]
     
     # Download a JSON for each State in State Names.
     for st in statelist:
-        url = f"https://static01.nyt.com/elections-assets/2020/data/api/2020-11-03/race-page/{st}/president.json"
-        r = requests.get(url, allow_redirects=True)
-        open(f"{st}.json", "wb").write(r.content)
+        try:
+            with urllib.request.urlopen(f"https://static01.nyt.com/elections-assets/2020/data/api/2020-11-03/race-page/{st.lower()}/president.json") as url:
+                data = json.loads(url.read().decode())
+                with open(f"{st}.json", "w") as outfile:
+                    json.dump(data, outfile)
+        except:
+            print(f"{st}")
+            pass
+
+if __name__ == "__main__":
+    downloadvoterjson()
